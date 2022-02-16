35dca52ac0e52b6bfaa0848f080a47429197e162
Andres Soca <lumen.development01@gmail.com>
2022-02-15 21:41:15 -0500

    frontend and backend completely set up, database and models created as well as associations done. Splash page component added and styled -ASoca

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import "./Form.css"
+
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +33,40 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <>
+    <div className="background__container"></div>
+      <div className='form__container'>
+        <form onSubmit={onLogin}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div className='form__top__text'>
+            <h1>Login</h1>
+          </div>
+          <div>
+            <input
+              name='email'
+              type='text'
+              placeholder='Email Address'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <input
+              name='password'
+              type='Password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+            <button type='submit'>Sign In</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </>
   );
 };
 
