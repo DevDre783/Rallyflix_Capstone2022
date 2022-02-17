8cc77dab590f01c9e06fde9a20a0deb05b7670e7
Andres Soca <lumen.development01@gmail.com>
2022-02-17 00:20:58 -0500

    finished videos seeds and completed migrating and seeding that data to the database

--- app/models/video.py
+++ app/models/video.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from .db import db
 
 class Video(db.Model):
@@ -5,8 +6,17 @@ class Video(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    summary = db.Column(db.String(280), nullable=False)
+    summary = db.Column(db.String(500), nullable=False)
     url = db.Column(db.String(280), nullable=False)
 
 
     list_info = db.relationship("List", back_populates="video_info")
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'summary': self.summary,
+            'url': self.url
+        }
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .profiles import seed_profiles, undo_profiles
+from .videos import seed_videos, undo_videos
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_profiles()
+    seed_videos()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_profiles()
+    undo_videos()
     # Add other undo functions here
