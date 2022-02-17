2cf2626d6b3cc8921eeb2b75b8a4c0072e60b54d
Andres Soca <lumen.development01@gmail.com>
2022-02-17 18:47:20 -0500

    test

---
+++ app/forms/add_profile_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from app.models import Profile
+
+
+class AddProfileForm(FlaskForm):
+    name = StringField("Profile name", validators=[DataRequired()])
--- app/seeds/lists.py
+++ app/seeds/lists.py
@@ -5,10 +5,11 @@ def seed_lists():
 
     list1 = List(title="My List", profile_id=1, video_id=22)
     list2 = List(title="Best of 2021", profile_id=1, video_id=16)
-    list3 = List(title="Love Rally!", profile_id=3, video_id=16)
+    list3 = List(title="Love Rally!", profile_id=2, video_id=16)
 
     db.session.add(list1)
     db.session.add(list2)
+    db.session.add(list3)
 
     db.session.commit()
 
